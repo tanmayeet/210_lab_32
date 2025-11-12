05df33208c3937b149af663d3e3d8f20f8f081ec
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-282.local>
2025-11-12 00:09:54 -0800

    Setting up my code

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// COMSC210 | Lab 32 | Tanmayee Chalamalasetti
+// IDE Used: VS Code
+
+#include <deque>
+#include <iostream>
+
+#include "Car.h"
+
+const int initialSize = 2;
+
+int main() {
+  deque<Car> myDeque(initialSize);
+
+  return 0;
+}
