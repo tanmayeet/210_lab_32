396b7400dfed42dd1649d9cdc105f5dc478d6cbf
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-282.local>
2025-11-15 23:22:48 -0800

    Working on Milestone 2: Updating loops to accept arrays

--- main.cpp
+++ main.cpp
@@ -14,8 +14,7 @@ const int randMax = 100;
 const int numLanes = 4;
 
 int main() {
-  deque<Car> tollLane;
-
+  // deque<Car> tollLane;
   deque<Car> tollPlaza[numLanes];
   // Used to randomize i for a probability
   srand(time(0));
@@ -29,55 +28,62 @@ int main() {
 
   // Prints initial queue using a range-based for loop
   cout << "Initial queue:\n";
-  for (auto& car : tollLane) {
-    cout << "   ";
-    car.print();
+  for (int i = 0; i < numLanes; i++) {
+    cout << "Lane " << i << ": ";
+    for (auto& car : tollPlaza[i]) {
+      cout << "   ";
+      car.print();
+    }
+    cout << "\n";
   }
-  cout << "\n";
 
   int time = 1;
-  while (!tollLane.empty()) {
-    // i is for deciding the probability of whether the car in the front paid or
-    // another car joined the queue
-    int i = (rand() % randMax);
+  for (int i = 0; i < numLanes; i++) {
+    while (!tollPlaza[i].empty()) {
+      // i is for deciding the probability of whether the car in the front paid
+      // or another car joined the queue
+      int i = (rand() % randMax);
 
-    cout << "Time: " << time << " Operation: ";
-    if (i < probPaidCar) {
-      // When testing my code, I realized that for this function, because I want
-      // it to print out the car that paid even if the queue is empty, I need to
-      // initialize a new Car object and set it to the front car so I can print
-      // this value later instead of printing the tollLane at that time, which
-      // is empty
-      Car paidCar = tollLane.front();
-      // Changed this so after I set the object to the front car, then I pop the
-      // front car so it's not in the list anymore
-      tollLane.pop_front();
-      // print the car that paid
-      cout << "Car paid: ";
-      paidCar.print();
+      cout << "Time: " << time << " Operation: ";
+      if (i < probPaidCar) {
+        // When testing my code, I realized that for this function, because I
+        // want it to print out the car that paid even if the queue is empty, I
+        // need to initialize a new Car object and set it to the front car so I
+        // can print this value later instead of printing the tollLane at that
+        // time, which is empty
+        Car paidCar = tollLane.front();
+        // Changed this so after I set the object to the front car, then I pop
+        // the front car so it's not in the list anymore
+        tollLane.pop_front();
+        // print the car that paid
+        cout << "Car paid: ";
+        paidCar.print();
 
-    } else {
-      // Because this function is not getting rid of any cars, we don't need to
-      // technically set a new Car object to the back car that just joined the
-      // line, but we technicallly could have the same format as the initial
-      // loop if we wanted to
-      tollLane.push_back(Car());
-      // Generates random car object from class in Car.h
-      cout << "Joined lane: ";
-      tollLane.back().print();
+      } else {
+        // Because this function is not getting rid of any cars, we don't need
+        // to technically set a new Car object to the back car that just joined
+        // the line, but we technicallly could have the same format as the
+        // initial loop if we wanted to
+        tollLane.push_back(Car());
+        // Generates random car object from class in Car.h
+        cout << "Joined lane: ";
+        tollLane.back().print();
+      }
     }
 
     // Printing out the queue at a given time interval
     cout << "Queue: \n";
-    if (tollLane.empty()) {
-      cout << "    Empty";
+    for (int i = 0; i < numLanes; i++) {
+      if (tollPlaza[].empty()) {
+        cout << "    Empty";
 
-    } else {
-      for (auto& car : tollLane) {
-        cout << "   ";
-        car.print();
+      } else {
+        for (auto& car : tollLane) {
+          cout << "   ";
+          car.print();
+        }
+        cout << "\n";
       }
-      cout << "\n";
     }
     time++;
   }
